6e98404919f15bad4e081e884622ba380b3aedd0
geibatista <geibatista442@gmail.com>
2024-08-06 21:32:25 -0300

    Estrutura condicional ternária encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,7 +2,7 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Prova de recuperação" : "Reprovado";
 
         System.out.println(resultado);
     }
