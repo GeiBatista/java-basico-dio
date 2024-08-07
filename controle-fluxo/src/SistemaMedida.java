6d5170814b45d046291d84d4f6907579c4dc41bc
geibatista <geibatista442@gmail.com>
2024-08-06 21:53:28 -0300

    Estrutura condicional switchCase

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,21 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+
+        String tamanho = "m";
+
+        switch (tamanho) {
+            case "p":
+                System.out.println("Pequeno");
+                break;
+            case "m":
+                System.out.println("Médio");
+                break;
+            case "g":
+                System.out.println("Grande");
+                break;
+            default:
+                System.out.println("Indefinido");
+                break;
+        }
+    }
+}
