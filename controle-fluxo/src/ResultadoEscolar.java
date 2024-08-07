5b16488ca3aba3bfa4134fceb94253d13b167482
geibatista <geibatista442@gmail.com>
2024-08-06 21:29:20 -0300

    Condicional ternária

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,15 +2,9 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
-        if(nota >= 7)
-          System.out.println("Aprovado");
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
 
-          else if(nota >= 5 && nota < 7)
-          System.out.println("Prova de recuperação");
-
-        else
-         System.out.println("Reprovado");
-
+        System.out.println(resultado);
     }
     
 }
