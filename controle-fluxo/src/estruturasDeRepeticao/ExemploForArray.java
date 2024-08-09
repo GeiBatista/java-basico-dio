e5925d3f57755de1844da06ef33c90bbb9ff1f1e
geibatista <geibatista442@gmail.com>
2024-08-08 21:50:24 -0300

    Estrutura de repetição For tradiiconal em arrays

---
+++ controle-fluxo/src/estruturasDeRepeticao/ExemploForArray.java
@@ -0,0 +1,14 @@
+package estruturasDeRepeticao;
+
+public class ExemploForArray {
+    public static void main(String[] args) {
+
+        String irmaos [] = {"Paulo", "Jairo", "Emília", "Moisés"};
+
+        for(int x = 0; x < irmaos.length; x ++){
+            System.out.println("O irmão no índice " + x + " é: " + irmaos[x]);
+        }
+        
+    }
+    
+}
