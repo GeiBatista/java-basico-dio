e05f14b11fd5b7dd4bb67f2332189808ee0a2587
geibatista <geibatista442@gmail.com>
2024-08-11 20:26:31 -0300

    Estrutua de repetição while

---
+++ controle-fluxo/src/estruturasDeRepeticao/ExemploWhile.java
@@ -0,0 +1,24 @@
+package estruturasDeRepeticao;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile {
+    public static void main(String[] args) {
+
+        double mesada = 50.00;
+        while(mesada > 0){
+            Double valorDoce = valorAleatorio();
+            if(valorDoce > mesada)
+                valorDoce = mesada;
+
+                System.out.println("Valor do doce: " + valorDoce + " Adicionado no carrinho");
+                mesada = mesada - valorDoce;
+            }
+            System.out.println("Mesada:" + mesada);
+            System.out.println("Joãosinho gastou toda a sua mesada em doces");
+        }
+    
+    private static double valorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble(2, 8);
+    }
+}
