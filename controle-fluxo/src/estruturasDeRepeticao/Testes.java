154a75fd47fb55641d44146b4aea0f9bdc81316b
geibatista <geibatista442@gmail.com>
2024-08-14 21:39:14 -0300

    Fluxo não interrompido com uso do bloco try/catch

---
+++ controle-fluxo/src/estruturasDeRepeticao/Testes.java
@@ -0,0 +1,11 @@
+package estruturasDeRepeticao;
+
+public class Testes {
+    public static void main(String[] args) {
+        boolean condicao = false;
+
+        while(condicao){
+            System.out.println("executou ... ");
+        }
+    }
+}
