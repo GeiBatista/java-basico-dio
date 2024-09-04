e871cb3fb917af921c5effaa0bc1a96e14c07eb5
geibatista <geibatista442@gmail.com>
2024-09-03 21:16:31 -0300

    Selecionando Candidatos - uso do While

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,34 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
 	public static void main(String[] args) {
-		analisarCandidatura(1900.0);		
-		analisarCandidatura(2200.0);		
-		analisarCandidatura(2000.0);		
+		selecaoCandidato();		
+	}
+	
+	static void selecaoCandidato() {
+		String [] candidatos = {"GEI","PAULO","JAIRO","EMÍLIA","MOISÉS","BÁRBARA","LUIZA","LUNA","VINÍCIUS","GEISINHA"};
+		
+		int candidatoSelecionado = 0;
+		int candidatoAtual=0;
+		double salarioBase = 2000.0;
+		while(candidatoSelecionado < 5 && candidatoAtual < candidatos.length) {
+			String candidato = candidatos[candidatoAtual];
+			double salarioPretendido = valorPretendido();
+			
+			System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido );
+			if(salarioBase >= salarioPretendido) {
+				System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+				candidatoSelecionado++;
+			}
+			candidatoAtual++;
+		}
+	}
+	
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 	
 	static void analisarCandidatura(double salarioPretendido) {
